96e5531629c1f9a84184a74c2dc2cc381a5aaea5
Barney de Villiers <barney.de.villiers@hetzner.co.za>
2016-05-23 14:02:55 +0200

    Adding connection error cases

--- lib/logstash_auditor/auditor.rb
+++ lib/logstash_auditor/auditor.rb
@@ -4,17 +4,18 @@ require "soar_auditor_api"
 module LogstashAuditor
   class LogstashAuditor < SoarAuditorApi::SoarAuditorAPI
 
-    def configuration_is_valid(configuration = {})
-      required_parameters = ["host_url", "use_ssl", "username", "password"]
+    #inversion of control method required by the SoarAuditorAPI
+    def configuration_is_valid(configuration)
+      required_parameters = ["host_url", "username", "password"]
       required_parameters.each { |parameter| return false unless configuration.include?(parameter) }
       return true
-
-      #TODO Remove the use ssl paramter by better looking at the url.
     end
 
-    def audit(data)
-      data = { "message" => data }
-      send_event( data )
+    #inversion of control method required by the SoarAuditorAPI
+    def audit(audit_data)
+      request = create_request(audit_data)
+      http    = create_http_transport
+      send_request_to_server(http, request)
     end
 
     private
@@ -22,35 +23,23 @@ module LogstashAuditor
     def create_http_transport
       uri = URI.parse(@configuration["host_url"])
       http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true if uri.is_a?(URI::HTTPS)
       http.read_timeout = @configuration["timeout"]
       http.open_timeout = @configuration["timeout"]
-
-      if @configuration["use_ssl"]
-        http.use_ssl = true
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      end
-      return http, uri
+      return http
     end
 
-    def send_event(data)
-      http, uri = create_http_transport
-
-
-      request = Net::HTTP::Post.new(uri.request_uri, initheader = {'Content-Type' =>'application/json'})
+    def create_request(audit_data)
+      request = Net::HTTP::Post.new("/", initheader = {'Content-Type' => 'application/json'})
       request.basic_auth(@configuration["username"], @configuration["password"])
-      request.body = data.to_json
+      request.body = audit_data
+      return request
+    end
 
-      response = http.request(request)
-
-      case response.code
-      when "200"
-        return :success
-      when "401"
-        puts "Authorization failure contacting to logstash"
-      else
-        puts "Failure " + response.code + " communicating with logstash"
-      end
-      return :failure
+    def send_request_to_server(http, request)
+      response = http.request(request) rescue nil
+      raise StandardError, 'Failed to create connection' if response.nil?
+      raise StandardError, "Server rejected post with error code #{response.code}" unless response.code == "200"
     end
   end
 end
--- spec/logstash_auditor_spec.rb
+++ spec/logstash_auditor_spec.rb
@@ -3,15 +3,24 @@ require 'spec_helper'
 describe LogstashAuditor do
   before :all do
     @iut = LogstashAuditor::LogstashAuditor.new
-    @invalid_logstash_configuration = { "bla" => "bla"}
+    @invalid_logstash_configuration = { "foo" => "bar"}
     @valid_logstash_configuration = { "host_url" => "http://localhost:8080",
-                              "use_ssl"  => false,
-                              "username" => "something",
-                              "password" => "something",
-                              "timeout"  => 3}
-
+                                      "username" => "auditorusername",
+                                      "password" => "auditorpassword",
+                                      "timeout"  => 3}
+    @logstash_configuration_with_incorrect_port = { "host_url" => "http://localhost:9090",
+                                                    "username" => "auditorusername",
+                                                    "password" => "auditorpassword",
+                                                    "timeout"  => 3}
+    @logstash_configuration_with_incorrect_host = { "host_url" => "http://somewhere:8080",
+                                                    "username" => "auditorusername",
+                                                    "password" => "auditorpassword",
+                                                    "timeout"  => 3}
+    @logstash_configuration_with_incorrect_user = { "host_url" => "http://localhost:8080",
+                                                    "username" => "wrongauditorusername",
+                                                    "password" => "auditorpassword",
+                                                    "timeout"  => 3}
     @iut.configure(@valid_logstash_configuration)
-
     @elasticsearch = LogstashAuditor::ElasticSearchTestAPI.new('http://localhost:9200')
   end
 
@@ -19,20 +28,20 @@ describe LogstashAuditor do
     expect(LogstashAuditor::VERSION).not_to be nil
   end
 
-  context "when configuring" do
-    it 'should accept a valid auditor configuration' do
+  context "when configured by SoarAuditorAPI" do
+    it 'should accept a valid configuration' do
       expect(@iut.configuration_is_valid(@valid_logstash_configuration)).to eq(true)
     end
 
-    it 'should reject an invalid auditor configuration' do
+    it 'should reject an invalid configuration' do
       expect(@iut.configuration_is_valid(@invalid_logstash_configuration)).to eq(false)
     end
   end
 
-  context "when given audit" do
+  context "when asked by SoarAuditorAPI to audit" do
     it "should submit audit to logstash with data received" do
       #Create an unique test identifier that will be used to correlate the submitted test audit
-      #with the located message in elastic search.
+      #with the audit found by elastic search.
       test_identifier = @elasticsearch.create_test_id
 
       debug_message = "some audit event message"
@@ -45,6 +54,25 @@ describe LogstashAuditor do
       expect(found_event_message.include?(debug_message)).to eq(true) #Check if the correct audit message was stored
     end
 
-    it "should extract the level, flow, time and message fields on logstash server into separate fields"
+    it "should raise StandardError if logstash connection fails, given incorrect port" do
+      expect {
+        @iut.configure(@logstash_configuration_with_incorrect_port)
+        @iut.audit("message")
+      }.to raise_error(StandardError, 'Failed to create connection')
+    end
+
+    it "should raise StandardError if logstash connection fails, given incorrect host" do
+      expect {
+        @iut.configure(@logstash_configuration_with_incorrect_host)
+        @iut.audit("message")
+      }.to raise_error(StandardError, 'Failed to create connection')
+    end
+
+    it "should raise StandardError if logstash authentication fails" do
+      expect {
+        @iut.configure(@logstash_configuration_with_incorrect_user)
+        @iut.audit("message")
+      }.to raise_error(StandardError, "Server rejected post with error code 401")
+    end
   end
 end
