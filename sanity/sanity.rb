591588fa17362feca50d30022ec690f2cf03b3b8
Barney de Villiers <barney.de.villiers@hetzner.co.za>
2016-05-30 14:49:35 +0200

    Logstash filtering of optional fields operational and example added

--- sanity/sanity.rb
+++ sanity/sanity.rb
@@ -14,8 +14,9 @@ class Main
     @iut.configure(@logstash_configuration)
     @iut.set_audit_level(:debug)
 
-    my_optional_field = SoarAuditingFormatter::Formatter.optional_field_format("mykey", "myfield")
-    @iut.debug(SoarAuditingFormatter::Formatter.format(:debug,'my-sanity-service-id',SecureRandom.hex(32),Time.now,"#{my_optional_field} test message with optional field"))
+    my_optional_operation_field = SoarAuditingFormatter::Formatter.optional_field_format("operation", "Http.Get")
+    my_optional_method_name_field = SoarAuditingFormatter::Formatter.optional_field_format("method", "#{self.class}::#{__method__}::#{__LINE__}")
+    @iut.debug(SoarAuditingFormatter::Formatter.format(:debug,'my-sanity-service-id',SecureRandom.hex(32),Time.now.iso8601(3),"#{my_optional_method_name_field}#{my_optional_operation_field} test message with optional fields"))
   end
 end
 
