d8997419f88c2413ddebcf7714e62c50079fa671
Barney de Villiers <barney.de.villiers@hetzner.co.za>
2016-08-31 08:11:21 +0200

    Fixup the support scripts

--- spec/support/certificates/import_to_server_keystore.sh
+++ spec/support/certificates/import_to_server_keystore.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
-
 PASSWORD=logstash
 
 rm -f serverkeystore.jks
-keytool -import -noprompt -storepass $PASSWORD -keystore serverkeystore.jks -file root_ca/certs/root.ca.cert.pem                 -alias root_ca
-keytool -import -noprompt -storepass $PASSWORD -keystore serverkeystore.jks -file intermediate_ca/certs/intermediate.ca.cert.pem -alias intermediate_ca
-keytool -import -noprompt -storepass $PASSWORD -keystore serverkeystore.jks -file selfsigned/selfsigned.cert.pem                 -alias logstash_client_manual
+
+#import the client and CA certificates as needed
+#keytool -import -noprompt -storepass $PASSWORD -keystore serverkeystore.jks -file root_ca/certs/root.ca.cert.pem                 -alias root_ca
+#keytool -import -noprompt -storepass $PASSWORD -keystore serverkeystore.jks -file intermediate_ca/certs/intermediate.ca.cert.pem -alias intermediate_ca
+#keytool -import -noprompt -storepass $PASSWORD -keystore serverkeystore.jks -file selfsigned/selfsigned.cert.pem                 -alias logstash_client_manual
 keytool -import -noprompt -storepass $PASSWORD -keystore serverkeystore.jks -file selfsigned/selfsigned_registered.cert.pem      -alias logstash_client_registered
 
 #Generate and import selfsigned server keypair
 keytool -genkey -keyalg RSA -alias logstash_server -storepass $PASSWORD -keypass $PASSWORD -keystore serverkeystore.jks -validity 365 -keysize 4096 -dname "CN=localhost, OU=Hetzner, O=Hetzner, L=Durbanville, S=Western Cape , C=ZA"
 
-
+#verify the contents of the keystore
 keytool -list -storepass $PASSWORD -keystore serverkeystore.jks
--- spec/support/certificates/setup_certificates_for_logstash_testing.sh
+++ spec/support/certificates/setup_certificates_for_logstash_testing.sh
@@ -5,7 +5,7 @@ BASE_FOLDER=$(pwd)
 #create the self signed client certificates that we will use to authenticate ourselves
 #with the ELK stack
 cd $BASE_FOLDER/spec/support/certificates/selfsigned
-./create_self_signed_certificates.sh
+./create_self_signed_certificate.sh
 
 #import the certificates to the keystore
 cd $BASE_FOLDER/spec/support/certificates
