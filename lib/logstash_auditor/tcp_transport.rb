dcf301dbd9972797f52c43beafacf6b8deb330ae
Barney de Villiers <barney.de.villiers@hetzner.co.za>
2016-11-10 12:46:44 +0200

    reuse of established connection

--- lib/logstash_auditor/tcp_transport.rb
+++ lib/logstash_auditor/tcp_transport.rb
@@ -9,23 +9,32 @@ module LogstashAuditor
     end
 
     def audit(audit_data)
+      @ssl_socket = create_ssl_socket unless @ssl_socket
+      @ssl_socket.puts JSON.dump(audit_data)
+    end
+
+    private
+
+    def create_ssl_socket
+      ssl_socket = OpenSSL::SSL::SSLSocket.new(tcp_socket, ssl_context)
+      ssl_socket.sync_close = true
+      ssl_socket.connect
+      ssl_socket
+    end
+
+    def tcp_socket
       socket = TCPSocket.new(remote_host, remote_port)
       socket.sync = true
+      socket
+    end
 
+    def ssl_context
       ssl_context = OpenSSL::SSL::SSLContext.new()
       ssl_context.cert = OpenSSL::X509::Certificate.new(@configuration['certificate'])
       ssl_context.key = OpenSSL::PKey::RSA.new(@configuration['private_key'])
       ssl_context.ssl_version = :SSLv23
-
-      ssl_socket = OpenSSL::SSL::SSLSocket.new(socket, ssl_context)
-      ssl_socket.sync_close = true
-      ssl_socket.connect
-
-      ssl_socket.puts JSON.dump(audit_data)
-      ssl_socket.close
+      ssl_context
     end
-
-    private
 
     def remote_host
       host = URI.parse(@configuration['host_url']).host
