584899dbaa451afbd45baee842d9aca58f5c4c33
Barney de Villiers <barney.de.villiers@hetzner.co.za>
2017-08-01 08:29:42 +0200

    Added sanity scripts of testing certificates

--- sanity/sanity.rb
+++ sanity/sanity.rb
@@ -4,26 +4,75 @@ require 'time'
 require 'securerandom'
 
 class Main
-  def test_sanity
+
+  #2016
+  def test_sanity_using_previous_cert_on_staging
     @iut = LogstashAuditor::LogstashAuditor.new
-    @logstash_configuration = { "host_url"    => "https://localhost:8080",
-                                "certificate"  => File.read("../spec/support/certificates/selfsigned/selfsigned_registered.cert.pem"),
-                                "private_key" => File.read("../spec/support/certificates/selfsigned/selfsigned_registered.private.nopass.pem"),
+    @logstash_configuration = { "host_url"    => "https://logstash-staging1.jnb1.host-h.net:8080",
+      "certificate"  => File.read("/home/barney/Documents/20160909_logstash_production_client_certificate/soar.logstash.production.client.cert.pem"),
+      "private_key"  => File.read("/home/barney/Documents/20160909_logstash_production_client_certificate/soar.logstash.production.client.private.nopass.pem"),
                                 "timeout"     => 3}
     @iut.configure(@logstash_configuration)
     @iut.set_audit_level(:debug)
 
-    my_optional_operation_field = SoarAuditingFormatter::Formatter.optional_field_format("operation", "Http.Get")
-    my_optional_method_name_field = SoarAuditingFormatter::Formatter.optional_field_format("method", "#{self.class}::#{__method__}::#{__LINE__}")
-    @iut.debug(SoarAuditingFormatter::Formatter.format(:debug,'my-sanity-service-id',SecureRandom.hex(32),Time.now.iso8601(3),"#{my_optional_method_name_field}#{my_optional_operation_field} test message with optional fields"))
+    @iut.debug(SoarAuditingFormatter::Formatter.format(:debug,'my-sanity-service-id',SecureRandom.hex(32),Time.now.iso8601(3),"test message without analytics field"))
+    puts "Passed 2016 cert with staging"
+  rescue
+    puts "Failed 2016 cert with staging"
+  end
 
-    my_optional_analytics_field = SoarAuditingFormatter::Formatter.optional_field_format("soar_time_value", rand() * 10)
-    @iut.debug(SoarAuditingFormatter::Formatter.format(:debug,'my-sanity-service-id',SecureRandom.hex(32),Time.now.iso8601(3),"#{my_optional_analytics_field}test message with analytics field"))
+  #2017
+  def test_sanity_on_staging
+    @iut = LogstashAuditor::LogstashAuditor.new
+    @logstash_configuration = { "host_url"    => "https://logstash-staging1.jnb1.host-h.net:8080",
+                                "certificate"  => File.read("/home/barney/Documents/20170621_logstash_production_and_staging_client_certificates/soar.logstash.staging.client.cert.pem"),
+                                "private_key"  => File.read("/home/barney/Documents/20170621_logstash_production_and_staging_client_certificates/soar.logstash.staging.client.private.nopass.pem"),
+                                "timeout"     => 3}
+    @iut.configure(@logstash_configuration)
+    @iut.set_audit_level(:debug)
 
     @iut.debug(SoarAuditingFormatter::Formatter.format(:debug,'my-sanity-service-id',SecureRandom.hex(32),Time.now.iso8601(3),"test message without analytics field"))
+    puts "Passed 2017 cert with staging"
+  rescue
+    puts "Failed 2017 cert with staging"
+  end
 
+  #2016
+  def test_sanity_using_previous_cert_on_production
+    @iut = LogstashAuditor::LogstashAuditor.new
+    @logstash_configuration = { "host_url"    => "https://logstash.jnb1.host-h.net:8080",
+                                "certificate"  => File.read("/home/barney/Documents/20160909_logstash_production_client_certificate/soar.logstash.production.client.cert.pem"),
+                                "private_key"  => File.read("/home/barney/Documents/20160909_logstash_production_client_certificate/soar.logstash.production.client.private.nopass.pem"),
+                                "timeout"     => 3}
+    @iut.configure(@logstash_configuration)
+    @iut.set_audit_level(:debug)
+
+    @iut.debug(SoarAuditingFormatter::Formatter.format(:debug,'my-sanity-service-id',SecureRandom.hex(32),Time.now.iso8601(3),"test message without analytics field"))
+    puts "Passed 2016 cert with production"
+  rescue
+    puts "Failed 2016 cert with production"
+  end
+
+  #2017
+  def test_sanity_on_production
+
+    @iut = LogstashAuditor::LogstashAuditor.new
+    @logstash_configuration = { "host_url"    => "https://logstash.jnb1.host-h.net:8080",
+                                "certificate"  => File.read("/home/barney/Documents/20170621_logstash_production_and_staging_client_certificates/soar.logstash.production.client.cert.pem"),
+                                "private_key"  => File.read("/home/barney/Documents/20170621_logstash_production_and_staging_client_certificates/soar.logstash.production.client.private.nopass.pem"),
+                                "timeout"     => 3}
+    @iut.configure(@logstash_configuration)
+    @iut.set_audit_level(:debug)
+
+    @iut.debug(SoarAuditingFormatter::Formatter.format(:debug,'my-sanity-service-id',SecureRandom.hex(32),Time.now.iso8601(3),"test message without analytics field"))
+    puts "Passed 2017 cert with production"
+  rescue
+    puts "Failed 2017 cert with production"
   end
 end
 
 main = Main.new
-main.test_sanity
+main.test_sanity_using_previous_cert_on_production
+main.test_sanity_on_production
+main.test_sanity_using_previous_cert_on_staging
+main.test_sanity_on_staging
